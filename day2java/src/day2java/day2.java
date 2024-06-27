5e6c7af1adcd4190d501c43537db857bd12afdd5
a1aakash <146542301+a1aakash@users.noreply.github.com>
2024-06-27 11:42:51 +0530

    day2

---
+++ day2java/src/day2java/day2.java
@@ -0,0 +1,18 @@
+package day2java;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		char ch='a';
+				System.out.println(ch);
+		char ch1 = 65;
+		        System.out.println(ch1);
+		int a = 'A';
+		        System.out.println(a);
+		char var1 = '\u00A7';
+		        System.out.println(ch1);        
+		
+		
+	}
+
+}
