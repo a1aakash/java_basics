e1c3a530321ddf79d007988d070b4d695cdfd076
a1aakash <146542301+a1aakash@users.noreply.github.com>
2024-07-01 10:07:23 +0530

    day 1

---
+++ javabasics/src/javaday1/day12.java
@@ -0,0 +1,12 @@
+package javaday1;
+
+public class day12 {
+
+	public static void main(String[] args) {
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName); 
+		}
+
+	}
